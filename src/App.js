9638967de9a55830ccee9801d7d8b18b2ddfc21e
unknown <srivastavaabhishek104@gmail.com>
2019-07-10 16:44:01 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>This is My Heading</h1>
-      <h3>This is My Paragraph</h3>
-      <Person name="Abhishek" age="26">My Hobbies: Playing and Sleeping</Person>
-      <Person name="Deepak" age="24"></Person>
-      <Person name="Tarun" age="25"></Person>
-    </div>
-  );
+class App extends Component {
+	state = {
+		person : [
+		{name: "Abhishek", age: 26},
+		{name: "Deepak", age: 24},
+		{name: "Tarun", age: 25},
+		{name: "Atishay", age: 26}
+		]
+	};
+  	render(){
+		return(
+			<div className="App">
+				<h1>This is My Heading</h1>
+				<h3>This is My Paragraph</h3>
+				{
+					this.state.person.map((elementItem)=>{
+						return <Person name = {elementItem.name} age={elementItem.age}></Person>
+					})
+				}
+			</div>
+		);
+  	}
 }
 
 export default App;
